24b5f5679a831cd9794ef19e7cf277cd4f8facb7
jyotirmaysenapati <senapati.jyotirmay@gmail.com>
2020-09-22 06:27:14 +0200

    22092020: kloserr added + loader modified a bit.

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,9 +1,10 @@
 
 $(document).ready(function() {
-
-	setTimeout(function(){
-		$('body').addClass('loaded');
-		$('h1').css('color','#222222');
-	}, 3000);
+	$('body').addClass('loaded');
+	$('h1').css('color','#000000');
+	// setTimeout(function(){
+	// 	$('body').addClass('loaded');
+	// 	$('h1').css('color','#000000');
+	// }, 1000);
 
 });
